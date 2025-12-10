d85b14eaf7a55e9e9152e906e40f746cbd0cb25a
igas1234 <fabregasandro@gmail.com>
2025-12-10 16:02:44 +0700

    "feat: tambah fitur login"

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimeApiController;
-
+use App\Http\Controllers\LoginController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -16,3 +16,13 @@ Route::prefix('api')->group(function () {
     Route::get('/watch', [AnimeApiController::class, 'watch']);
     Route::post('/cache/clear', [AnimeApiController::class, 'clearCache']);
 });
+
+
+Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
+
+
