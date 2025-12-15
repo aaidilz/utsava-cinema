67bd8b623508602d09b9e5def2103357ea4d5624
aaidilz <aidil.233040059@mail.unpas.ac.id>
2025-12-15 20:11:41 +0700

    refactor: remove broken Anime API routes from web.php

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-#pastiin harus pake middleware auth biar ga sembarangan orang bisa akses :D
-// Anime API Routes
-Route::prefix('api')->group(function () {
-    Route::get('/search', [AnimeApiController::class, 'search']);
-    Route::get('/episodes', [AnimeApiController::class, 'episodes']);
-    Route::get('/watch', [AnimeApiController::class, 'watch']);
-    Route::post('/cache/clear', [AnimeApiController::class, 'clearCache']);
-});
-
-
 Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout']);
