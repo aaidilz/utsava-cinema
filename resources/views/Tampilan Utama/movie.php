f831ba46bcbe5e20d14f70839e941aa45b786814
fridaykiller2 <rizaalif464@gmail.com>
2025-12-15 14:39:38 +0700

    Tampilan Utama

--- resources/views/Tampilan Utama/movie.php
+++ resources/views/Tampilan Utama/movie.php
@@ -8,31 +8,27 @@ $movies = query("SELECT * FROM movies");
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
-    />
     <link rel="stylesheet" href="movie/style.css" />
     <link
       rel="shortcut icon"
       href="img/logo belakang.jpg"
       type="image/x-icon"
     />
-    <title>Movies</title>
+    <title>Utsava-Cinema</title>
   </head>
 
   <body>
     <header>
-      <video src="video/money.mp4" autoplay muted loop></video>
+      <video src="#" autoplay muted loop></video>
       <nav>
         <div class="logo_ul">
-          <img src="img/.jpg" alt="" />
+          <img src="#" alt="" />
           <ul>
             <li>
               <a href="movie.php">Home</a>
             </li>
             <li>
-              <a href="serial.php">Series</a>
+              <a href="#">Series</a>
             </li>
             <li>
               <a href="#">Movies</a>
@@ -44,14 +40,12 @@ $movies = query("SELECT * FROM movies");
         </div>
         <div class="search_user">
           <input type="text" placeholder="Search..." id="Search_input" />
-          <img src="img/user.jpg" alt="" />
-          <a href="login.php"> <button class="btn">log out</button></a>
           <div class="search">
             <?php if(isset($_SESSION['search'])) : ?>
             <a href="#" class="card">
-              <img src="img/ant-man-wasp1.jpg" alt="" />
+              <img src="#" alt="" />
               <div class="cont">
-                <h3>ant-man-wasp</h3>
+                <h3>#</h3>
                 <p>
                   Action, 2015, <span>IMDB</span
                   ><i class="bi bi-star-fill"></i> 9.8
@@ -63,24 +57,19 @@ $movies = query("SELECT * FROM movies");
         </div>
       </nav>
       <div class="content">
-        <h1 id="title">Money Heist</h1>
+        <h1 id="title">#</h1>
         <p>
-          Money Heist is a crime-themed television drama series from Spain.
-          Produced by Álex Pina, the series was originally intended as a limited
-          series to be told in two parts. The series originally had 15 episodes
-          which aired on the Spanish television network, Antena 3, from 2 May
-          2017 to 23 November 2017.
         </p>
         <div class="details">
-          <h6>A Netflix Original Film</h6>
-          <h5 id="gen">Thriller</h5>
-          <h4 id="date">2021</h4>
+          <h6>#</h6>
+          <h5 id="gen">#</h5>
+          <h4 id="date">#</h4>
           <h3 id="rate">
-            <span>IMDB</span><i class="bi bi-star-fill"></i> 9.8
+            <span>#</span><i class="bi bi-star-fill"></i> 9.8
           </h3>
         </div>
         <div class="btns">
-          <a href="#" id="play">watch <i class="bi bi-play-fill"></i></a>
+          <a href="#" id="play">#<i class="bi bi-play-fill"></i></a>
           <a href="#" id="download_main">
             <i class="bi bi-cloud-arrow-down-fill"></i>
           </a>
@@ -109,10 +98,5 @@ $movies = query("SELECT * FROM movies");
         </div>
       </section>
     </header>
-    <script src="movie/app.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
   </body>
 </html>
