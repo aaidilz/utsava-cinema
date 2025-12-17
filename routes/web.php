780a720bad6333df1fa7d8a5261fffa4c1520179
aaidilz <aidil.233040059@mail.unpas.ac.id>
2025-12-17 07:44:55 +0700

    feat: enhance HomeController to fetch and cache anime genres, update home view with swiper integration

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 
-Route::get('/', function () {
-    return view('home.index');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/login', [LoginController::class, 'showLogin'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
